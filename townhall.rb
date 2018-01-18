047e1662c946ffbfe5bf6e80374e31ea02d17d5f
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-18 13:48:50 +0100

    Scrapping par url fonctionnel

--- townhall.rb
+++ townhall.rb
@@ -5,18 +5,26 @@ require 'pry'
 class Scrapping
 
   def get_the_email_of_a_townhal_from_its_webpage(url)
+    my_hash = {}
     @user_url = url
     page = Nokogiri::HTML(open(@user_url))
+    mail_table = page.xpath("//td[@class = 'style27']")[5].text
+    name_table = page.xpath("//a[@class = 'lientxt4']")[1].text.capitalize
+    #J'enleve tout les espaces pour mon mail
+    @mail_table = mail_table.gsub(/\s+/, "")
+    my_hash[name_table] = @mail_table[1..-1]
+    return my_hash
+  end
 
-    mail_table = page.css("body > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td:nth-child(1) > table:nth-child(8) > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(4)")
-    name_table = page.xpath("//a[@class = 'lientxt4']")[1].text)
-#my_hash[name_table] = mail_table
-puts mail_table
-binding.pry
+def get_all_the_urls_of_val_doise_townhalls(url)
+  @url = url
+
 end
 
 end
 
+
+puts "This scrapping tool allows you to get the email for all the townhall in Val d'Oise (France)"
 #print "What is the website you want to scrap ?: "
 #user_input = gets.chomp.to_s
 
@@ -26,8 +34,8 @@ scrapper = Scrapping.new
 
 unless user_input.empty?
 
-  scrapper.get_the_email_of_a_townhal_from_its_webpage(user_input)
-  binding.pry
+  puts scrapper.get_the_email_of_a_townhal_from_its_webpage(user_input)
+
 else
   puts "So you don't want to scrap. Bye"
 end
